6081d7b1bc0e4166fa0e07a9bcd4d44f8ca8b13a
bajahranks <lamontagne.ravi@gmail.com>
2018-03-09 01:53:55 -0500

    Function for counting cards in a Blackjack game

---
+++ Counting_Cards.js
@@ -0,0 +1,36 @@
+/*****************************************************************************
+* Function for counting cards in a Blackjack game. If the every card between 2
+* and 6, add 1. For every card between 10 and A, minus 1. For cards 7,8,9 do 
+* nothing.
+******************************************************************************/
+
+var count = 0;
+
+function cc(card) {
+ 
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case 'J':
+    case 'Q':
+    case 'K':
+    case 'A':
+      count--;
+      break;
+    default: 
+      break;
+  }
+  if (count > 0) {
+	return count + " Bet";
+  } else {
+	return count + " Hold";
+  }
+  
+  
+}
